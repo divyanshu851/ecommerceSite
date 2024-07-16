d41844bad67b149fc7412ba87eb153be90df3aa8
Divyanshu Shrivastav <divyanshushrivastav4.ds@gmail.com>
2024-07-16 16:11:28 +0530

    ecommerceSite code with spring security resource server

--- src/main/java/org/example/ecommerceproject/controller/ProductController.java
+++ src/main/java/org/example/ecommerceproject/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.example.ecommerceproject.dtos.ProductResponseDTO;
 import org.example.ecommerceproject.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
